9a62badbdfb5e642590b784f86e584967c8bf6cc
odwyerr4 <odwerr4@tcd.ie>
2020-10-28 10:01:18 +0000

    Added more tests

--- LCATest.py
+++ LCATest.py
@@ -5,8 +5,6 @@ import LCA
 class TestLCA(unittest.TestCase):
 
     def test_LCA(self):
-        print ("testing")
-        
         root = LCA.Node(20) 
         root.left = LCA.Node(8) 
         root.right = LCA.Node(22) 
@@ -15,7 +13,9 @@ class TestLCA(unittest.TestCase):
         root.left.right.left = LCA.Node(10) 
         root.left.right.right = LCA.Node(14)
         self.assertEqual(LCA.lca(root, 10,14).data,12) 
-                
+        self.assertEqual(LCA.lca(root, 14,8).data,8) 
+        self.assertEqual(LCA.lca(root, 10,22).data, 20)  
+              
 
 if __name__ == "main":
     unittest.main()
